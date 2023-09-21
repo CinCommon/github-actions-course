51535a6de947dca2542234db1f2f5487caedff94
尹子凡 Steven <20436656+StevenYinKop@users.noreply.github.com>
2023-09-21 23:35:27 +0800

    Update GoogleMaps.vue [skip ci]

--- src/views/GoogleMaps.vue
+++ src/views/GoogleMaps.vue
@@ -5,18 +5,18 @@
       <b-row>
         <b-col xl="3" md="6" @click="showInputModal(0)">
           <stats-card
-              title="画线"
-                      type="gradient-red"
-                      sub-title="searchLocation(至少两个点)"
-                      icon="ni ni-active-40"
-                      role="button"
-                      class="mb-4 pointer-event">
+            title="画线"
+            type="gradient-red"
+            sub-title="searchLocation(至少两个点)"
+            icon="ni ni-active-40"
+            role="button"
+            class="mb-4 pointer-event">
           </stats-card>
         </b-col>
         <b-col xl="3" md="6" @click.prevent="showInputModal(1)">
           <stats-card title="标点"
                       type="gradient-orange"
-                      sub-title="传入完整的places数组即可，会自动解析其中的position"
+                      sub-title="places数组/经纬度/{lat,lng}"
                       icon="ni ni-chart-pie-35"
                       class="mb-4">
           </stats-card>
@@ -28,23 +28,24 @@
                       icon="ni ni-money-coins"
                       class="mb-4">
 
-            <template slot="footer">
-              <span class="text-danger mr-2">5.72%</span>
-              <span class="text-nowrap">Since last month</span>
-            </template>
           </stats-card>
-
         </b-col>
         <b-col xl="3" md="6">
-          <stats-card title="Performance"
+          <stats-card title="地图选点"
                       type="gradient-info"
-                      sub-title="49,65%"
+                      sub-title="点击城市/点击地图 -> 复制到剪切板"
                       icon="ni ni-chart-bar-32"
                       class="mb-4">
-
             <template slot="footer">
-              <span class="text-success mr-2">54.8%</span>
-              <span class="text-nowrap">Since last month</span>
+                <b-badge
+                  class="mx-1"
+                  v-for="(value, name) in placePositions"
+                         :key="name"
+                         href="#"
+                         variant="info"
+                         @click="copyPositionToClipboard(value)"
+                >{{ name }}
+                </b-badge>
             </template>
           </stats-card>
         </b-col>
@@ -62,209 +63,224 @@
     </b-container>
     <b-modal @ok.prevent="parseLocation" :id="locationInputModalTitle" no-close-on-backdrop>
       <template #modal-title>
-        {{modalTitle}}
+        {{ modalTitle }}
       </template>
       <div class="d-block">
-        <textarea id="code-editor" v-model="codeValue" ></textarea>
+        <textarea id="code-editor" v-model="codeValue"></textarea>
       </div>
     </b-modal>
   </div>
 </template>
 <script>
-  import { API_KEY } from './Maps/API_KEY';
-  import {TencentMap} from "ppos-vue-tencent-map/src/components";
-  import dedent from 'dedent'
-  import CodeMirror from "codemirror"
-  import 'codemirror/lib/codemirror.css';
-  import {decode} from "../util/PolylineDecoder";
-  import {checkInputValidation} from "../util/v5-json-parse";
+import {API_KEY} from './Maps/API_KEY';
+import {TencentMap} from "ppos-vue-tencent-map/src/components";
+import dedent from 'dedent'
+import CodeMirror from "codemirror"
+import 'codemirror/lib/codemirror.css';
+import {decode} from "../util/PolylineDecoder";
+import {checkInputValidation} from "../util/v5-json-parse";
 
-  const LINE = 0;
-  const MARKER = 1;
+const LINE = 0;
+const MARKER = 1;
+const PLACE_POSITION = {
+  "北京": {lat: 0.0, lng: 0.0},
+  "上海": {lat: 0.0, lng: 0.0},
+  "南京": {lat: 0.0, lng: 0.0},
+  "深圳": {lat: 0.0, lng: 0.0},
+  "香港": {lat: 0.0, lng: 0.0},
+  "澳门": {lat: 0.0, lng: 0.0},
+  "杭州": {lat: 0.0, lng: 0.0}
+};
 
-  const LOCATIONS_INPUT_MODAL_TITLE = "bv-modal-map-input"
+const LOCATIONS_INPUT_MODAL_TITLE = "bv-modal-map-input"
 
-  export default {
-    components: {
-      TencentMap
-    },
-    data() {
-      return {
-        apiKey: API_KEY,
-        locationInputModalTitle: LOCATIONS_INPUT_MODAL_TITLE,
-        code: dedent`
+export default {
+  components: {
+    TencentMap
+  },
+  data() {
+    return {
+      apiKey: API_KEY,
+      locationInputModalTitle: LOCATIONS_INPUT_MODAL_TITLE,
+      code: dedent`
           An h1 header
           ============
 
           Paragraphs are separated by a blank line.`,
-        cmOption: {
-          tabSize: 4,
-          styleActiveLine: true,
-          lineNumbers: true,
-          lineWrapping: false,
-          line: true,
-          mode: 'text/x-markdown',
-          theme: 'lesser-dark'
-        },
-        cm: undefined,
-        codeValue: '',
-        mapIsReady: false,
-        map: undefined,
-        TMap: undefined,
-        polylines: undefined,
-        labels: undefined,
-        markers: undefined,
-        places: [],
-        modalType: LINE
-      }
-    },
-    computed: {
-      modalTitle() {
-        switch (this.modalType) {
-          case LINE:
-            return "请输入searchLocation参数";
-          case MARKER:
-            return "请输入places数组";
-          default:
-            return "未知格式，可能无法使用"
-        }
-      }
-    },
-    methods: {
-      parseLocation() {
-        try {
-          switch (this.modalType) {
-            case LINE:
-              this.drawLine();
-              break;
-            case MARKER:
-              this.drawMarker();
-              break;
-            default:
-              break;
-          }
-        } catch (e) {
-          this.$bvToast.toast(`传入的参数无法解析！${e}`, {
-            title: '错误！',
-            variant: 'danger',
-            solid: true
-          })
-        }
+      cmOption: {
+        tabSize: 4,
+        styleActiveLine: true,
+        lineNumbers: true,
+        lineWrapping: false,
+        line: true,
+        mode: 'text/x-markdown',
+        theme: 'lesser-dark'
       },
-      drawMarker() {
-        const inputJson = checkInputValidation(JSON.parse(this.cm.getValue()));
-        const geometries = inputJson
-            .map(places => places.place)
-            .map(({providerId, providerPlaceId, position, title}) => ({
-              id: `${providerId}-${providerPlaceId}`, // 点图形数据的标志信息
-              styleId: 'label', // 样式id
-              position: new TMap.LatLng(position.lat, position.lng), // 标注点位置
-              content: title, // 标注文本
-              properties: {
-                // 标注点的属性数据
-                title: 'label',
-              },
-            }))
-        this.markers.setGeometries(geometries);
-        this.closeModal(
-            geometries.map(geometry => geometry.position)
-        );
-      },
-      drawLine() {
-        let routeArrays;
-        try {
-          routeArrays = decode(this.cm.getValue());
-        } catch (e) {
-          try {
-            routeArrays = this.cm.getValue()
-                .split("|")
-                .map(location => location.split(",")).map(latLng => ({
-                  lat: latLng[0],
-                  lng: latLng[1]
-                }));
-          } catch (e) {
-            routeArrays = [];
-            throw e;
-          }
-        }
-        if (routeArrays.length < 2) {
-          throw '路径至少存在两个点！';
-        }
-        this.polylines.remove('pl_1');
-        const latLngArray = routeArrays.map(item => new TMap.LatLng(item.lat, item.lng));
-        this.polylines.add({
-          'id': 'pl_1',//折线唯一标识，删除时使用
-          'styleId': 'style_blue',//绑定样式名
-          'paths': latLngArray
-        })
-        this.closeModal(latLngArray);
-      },
-      closeModal(latLngs) {
-        this.$nextTick(() => {
-          const latlngBounds = new TMap.LatLngBounds();
-          latLngs.forEach(item => latlngBounds.extend(item))
-          this.map.fitBounds(latlngBounds);
-          this.$bvModal.hide(LOCATIONS_INPUT_MODAL_TITLE)
-        })
-      },
-      showInputModal(modalType) {
-        this.$bvModal.show(LOCATIONS_INPUT_MODAL_TITLE);
-        this.$nextTick().then(() => {
-          this.modalType = modalType;
-          this.cm = CodeMirror.fromTextArea(document.getElementById('code-editor'), this.cmOption)
-        });
-      },
-      onMapReady({TMap, map}) {
-        this.mapIsReady = true;
-        this.map = map;
-        this.TMap = TMap;
-        this.polylines = new TMap.MultiPolyline({
-          id: 'polyline-layer', //图层唯一标识
-          map,//设置折线图层显示到哪个地图实例中
-          styles: {
-            'style_blue': new TMap.PolylineStyle({
-              'color': '#3777FF', //线填充色
-              'width': 6, //折线宽度
-              'borderWidth': 5, //边线宽度
-              'borderColor': '#FFF', //边线颜色
-              'lineCap': 'butt' //线端头方式
-            })
-          }, //折线样式定义
-          geometries: [] //折线数据定义
-        });
-        this.labels = new TMap.MultiLabel({
-          id: 'label-layer',
-          map: map,
-          styles: {
-            label: new TMap.LabelStyle({
-              color: '#3777FF', // 颜色属性
-              size: 20, // 文字大小属性
-              offset: {x: 0, y: 0}, // 文字偏移属性单位为像素
-              angle: 0, // 文字旋转属性
-              alignment: 'center', // 文字水平对齐属性
-              verticalAlignment: 'middle', // 文字垂直对齐属性
-            }),
-          },
-          geometries: []
-        });
-        this.markers = new TMap.MultiMarker({
-          id: 'marker-layer',
-          map: map,
-          styles: {
-            "myStyle": new TMap.MarkerStyle({
-              "width": 25,  // 点标记样式宽度（像素）
-              "height": 35, // 点标记样式高度（像素）
-              // "src": '../img/marker.png',  //图片路径
-              //焦点在图片中的像素位置，一般大头针类似形式的图片以针尖位置做为焦点，圆形点以圆心位置为焦点
-              "anchor": {x: 16, y: 32}
-            })
-          },
-          //点标记数据数组
-          geometries: []
-        });
-      },
-      mounted() {
+      clickCopyToggle: false,
+      placePositions: PLACE_POSITION,
+      cm: undefined,
+      codeValue: '',
+      mapIsReady: false,
+      map: undefined,
+      TMap: undefined,
+      polylines: undefined,
+      labels: undefined,
+      markers: undefined,
+      places: [],
+      modalType: LINE
+    }
+  },
+  computed: {
+    modalTitle() {
+      switch (this.modalType) {
+        case LINE:
+          return "请输入searchLocation参数";
+        case MARKER:
+          return "请输入places数组";
+        default:
+          return "未知格式，可能无法使用"
       }
     }
-  };
+  },
+  methods: {
+    parseLocation() {
+      try {
+        switch (this.modalType) {
+          case LINE:
+            this.drawLine();
+            break;
+          case MARKER:
+            this.drawMarker();
+            break;
+          default:
+            break;
+        }
+      } catch (e) {
+        this.$bvToast.toast(`传入的参数无法解析！${e}`, {
+          title: '错误！',
+          variant: 'danger',
+          solid: true
+        })
+      }
+    },
+    drawMarker() {
+      const inputJson = checkInputValidation(JSON.parse(this.cm.getValue()));
+      const geometries = inputJson
+        .map(places => places.place)
+        .map(({providerId, providerPlaceId, position, title}) => ({
+          id: `${providerId}-${providerPlaceId}`, // 点图形数据的标志信息
+          styleId: 'label', // 样式id
+          position: new TMap.LatLng(position.lat, position.lng), // 标注点位置
+          content: title, // 标注文本
+          properties: {
+            // 标注点的属性数据
+            title: 'label',
+          },
+        }))
+      this.markers.setGeometries(geometries);
+      this.closeModal(
+        geometries.map(geometry => geometry.position)
+      );
+    },
+    drawLine() {
+      let routeArrays;
+      try {
+        routeArrays = decode(this.cm.getValue());
+      } catch (e) {
+        try {
+          routeArrays = this.cm.getValue()
+            .split("|")
+            .map(location => location.split(",")).map(latLng => ({
+              lat: latLng[0],
+              lng: latLng[1]
+            }));
+        } catch (e) {
+          routeArrays = [];
+          throw e;
+        }
+      }
+      if (routeArrays.length < 2) {
+        throw '路径至少存在两个点！';
+      }
+      console.log(routeArrays.map(route => `${route.lat},${route.lng}`).join("|"));
+      this.polylines.remove('pl_1');
+      const latLngArray = routeArrays.map(item => new TMap.LatLng(item.lat, item.lng));
+      this.polylines.add({
+        'id': 'pl_1',//折线唯一标识，删除时使用
+        'styleId': 'style_blue',//绑定样式名
+        'paths': latLngArray
+      })
+      this.closeModal(latLngArray);
+    },
+    closeModal(latLngs) {
+      this.$nextTick(() => {
+        const latlngBounds = new TMap.LatLngBounds();
+        latLngs.forEach(item => latlngBounds.extend(item))
+        this.map.fitBounds(latlngBounds);
+        this.$bvModal.hide(LOCATIONS_INPUT_MODAL_TITLE)
+      })
+    },
+    showInputModal(modalType) {
+      this.$bvModal.show(LOCATIONS_INPUT_MODAL_TITLE);
+      this.$nextTick().then(() => {
+        this.modalType = modalType;
+        this.cm = CodeMirror.fromTextArea(document.getElementById('code-editor'), this.cmOption)
+      });
+    },
+    copyPositionToClipboard({lat, lng}) {
+      console.log('copyPositionToClipboard', lat, lng);
+    },
+    onMapReady({TMap, map}) {
+      this.mapIsReady = true;
+      this.map = map;
+      this.TMap = TMap;
+      this.polylines = new TMap.MultiPolyline({
+        id: 'polyline-layer', //图层唯一标识
+        map,//设置折线图层显示到哪个地图实例中
+        styles: {
+          'style_blue': new TMap.PolylineStyle({
+            'color': '#3777FF', //线填充色
+            'width': 6, //折线宽度
+            'borderWidth': 5, //边线宽度
+            'borderColor': '#FFF', //边线颜色
+            'lineCap': 'butt' //线端头方式
+          })
+        }, //折线样式定义
+        geometries: [] //折线数据定义
+      });
+      this.labels = new TMap.MultiLabel({
+        id: 'label-layer',
+        map: map,
+        styles: {
+          label: new TMap.LabelStyle({
+            color: '#3777FF', // 颜色属性
+            size: 20, // 文字大小属性
+            offset: {x: 0, y: 0}, // 文字偏移属性单位为像素
+            angle: 0, // 文字旋转属性
+            alignment: 'center', // 文字水平对齐属性
+            verticalAlignment: 'middle', // 文字垂直对齐属性
+          }),
+        },
+        geometries: []
+      });
+      this.markers = new TMap.MultiMarker({
+        id: 'marker-layer',
+        map: map,
+        styles: {
+          "myStyle": new TMap.MarkerStyle({
+            "width": 25,  // 点标记样式宽度（像素）
+            "height": 35, // 点标记样式高度（像素）
+            // "src": '../img/marker.png',  //图片路径
+            //焦点在图片中的像素位置，一般大头针类似形式的图片以针尖位置做为焦点，圆形点以圆心位置为焦点
+            "anchor": {x: 16, y: 32}
+          })
+        },
+        //点标记数据数组
+        geometries: []
+      });
+    },
+    mounted() {
+    }
+  }
+};
 </script>
